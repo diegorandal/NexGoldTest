0e5ac97303c31438dbc7080df19a815202ec9850
ChernobylNTF <wallydd1983@gmail.com>
2025-08-09 18:34:10 -0300

    Update index.ts

--- src/auth/wallet/index.ts
+++ src/auth/wallet/index.ts
@@ -37,7 +37,7 @@ export const walletAuth = async () => {
   }
 
 await signIn('credentials', {
-    redirectTo: '/Home',
+    redirectTo: false,
     nonce,
     signedNonce,
     finalPayloadJson: JSON.stringify(result.finalPayload),

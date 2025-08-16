71574618937e5f0ba7e28231c3b9c3b80c70213d
ChernobylNTF <wallydd1983@gmail.com>
2025-08-15 22:04:14 -0300

    Update page.tsx

--- src/app/(protected)/Home/page.tsx
+++ src/app/(protected)/Home/page.tsx
@@ -154,13 +154,13 @@ const StakingAndMiningSection: FC<{
 
             <div className="grid grid-cols-2 gap-4 text-center">
               <div>
-                <p className="text-sm text-gray-300">Recompensas Staking (APY)</p>
+                <p className="text-sm text-gray-300">Recompensas Mining</p>
                 <p className="text-xl font-bold text-green-400">
                   +{Number.parseFloat(contractData.stakingRewards).toFixed(4)} NXG
                 </p>
               </div>
               <div>
-                <p className="text-sm text-gray-300">Recompensas Mining (Fijo)</p>
+                <p className="text-sm text-gray-300">Recompensas Staking (APY/12%)</p>
                 <p className="text-xl font-bold text-yellow-400">
                   +{Number.parseFloat(contractData.miningRewards).toFixed(4)} NXG
                 </p>

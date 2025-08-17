4bb65498bb6b27884d8b7bf8ed8e7d441f4516a1
Diego <diegorandal@gmail.com>
2025-08-17 12:49:13 -0300

    update diegorandal

--- src/hooks/use-contract-data-ref.tsx
+++ src/hooks/use-contract-data-ref.tsx
@@ -44,7 +44,7 @@ export const useContractDataRef = () => {
       ])
 
       setContractDataRef({
-        rewardAmount: amount.toString(),
+        rewardAmount: formatEther(amount).toString(),
         rewardCount: count.toString(),
         isLoading: false,
       })

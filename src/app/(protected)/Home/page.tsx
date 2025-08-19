05f3cd189bb6802614662bcab97c258b5d9560bf
ChernobylNTF <wallydd1983@gmail.com>
2025-08-18 23:48:38 -0300

    Update page.tsx

--- src/app/(protected)/Home/page.tsx
+++ src/app/(protected)/Home/page.tsx
@@ -343,6 +343,9 @@ const StakingAndMiningSection: FC<{ onBack: () => void }> = ({ onBack }) => {
             <div className="grid grid-cols-2 gap-4"><GoldButton onClick={handleStake} disabled={isProcessing}>Stake</GoldButton><GoldButton onClick={handleUnstake} disabled={isProcessing || Number.parseFloat(contractData.availableBalance) <= 0}>Unstake</GoldButton></div>
             <GoldButton onClick={handleClaim} className="w-full" disabled={isProcessing || Number.parseFloat(contractData.stakedBalance) <= 0}>Reclamar Recompensas</GoldButton>
               <p className="text-center text-xs text-gray-400 pt-2">🚨 Si retiras tus fondos en staking antes de tiempo solo recibes el 50%, el resto es destinado a la quema (🔥) automática, manteniendo una economía estable.🚨</p>
+              <a href="https://t.me/+_zr0basq5yQ4ZmIx" target="_blank" rel="noopener noreferrer" className="self-center inline-block transition-transform duration-200 hover:scale-110">
+                 <img width="64" height="64" src="https://img.icons8.com/3d-fluency/94/telegram.png" alt="telegram"/>
+              </a>
           </>
         )}
         <div className="h-10 text-center text-sm flex items-center justify-center">

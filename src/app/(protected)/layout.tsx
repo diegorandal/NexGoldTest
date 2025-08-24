210e5a87feb0db8290fdb37c8551f8249c729b06
ChernobylNTF <wallydd1983@gmail.com>
2025-08-23 21:33:42 -0300

    Update layout.tsx

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -17,7 +17,12 @@ export default async function TabsLayout({
 
   return (
     <Page>
-      {children}
+      {/* Este div añade un padding inferior para que la barra de navegación fija no tape el contenido */}
+      <div className="pb-24">
+        {children}
+      </div>
+      
+      {/* El Footer ahora es fijo en la parte inferior y ocupa todo el ancho */}
       <Page.Footer className="fixed bottom-0 w-full bg-black pb-[env(safe-area-inset-bottom)]">
         <Navigation />
       </Page.Footer>
